c8dae6c1bb86ff0fefc5c4f9719c0ea8d7a9ff7c
Hugo Medeiros <hugoleonardobaldini@hotmail.com>
2019-04-29 15:18:07 -0300

    Segundo Commit

--- mod1/js/aula5.js
+++ mod1/js/aula5.js
@@ -1,7 +1,26 @@
-$(function(){
-    
-    $('img').error(function(){
-        $('img').attr('src','img/error.jpg')
-    })
+$(function () {
+
+    $('img')
+        .width($(window).width())
+        .height($(window).height())
+
+    $(window).resize(function () {
+        $('img')
+            .width($(window).width())
+            .height($(window).height())
+    });
+
+    // $(window).scroll(function () { 
+    //     $('img').fadeOut();
+    // });
+
+$(window).scroll(function () { 
+    var topo = $(window).scrollTop();
+    if(topo>10){
+        $('img').fadeOut(1000);
+    } else {
+        $('img').fadeIn();
+    }
+});
 
 })
