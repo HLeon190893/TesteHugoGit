4c0539ad0dd3a02ea75c4b152cdb7b37f79b0e75
Hugo Medeiros <hugoleonardobaldini@hotmail.com>
2019-04-29 16:15:58 -0300

    Terceiro Commit

--- mod1/js/aula6.js
+++ mod1/js/aula6.js
@@ -1,1 +1,26 @@
-    
+$(function () {
+
+    var ex = $('.ex1');
+
+    $('.ev1').click(function () {
+        $(this).css('background', '#ccc');
+        ex.text("Você Clicou!");
+    });
+
+    $('.ev2').dblclick(function () {
+        $(this).css('background', '#ccc');
+        ex.text("Você Clicou duas Vezes!");
+    });
+
+    $('.ev3')
+        .focusin(function () {
+            $(this).css('background', '#ccc')
+            ex.text("Você deu foco!")
+        })
+        .focusout(function () {
+            $(this).css('background', 'black')
+            ex.text("Você saiu do Foco!")
+        })
+
+
+})
